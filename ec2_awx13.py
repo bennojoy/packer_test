294daea418c98fd5875b8070e4456924629eef29
root <root@Ansible.(none)>
2013-10-04 08:12:09 +0530

    update 1

--- ec2_awx13.py
+++ ec2_awx13.py
@@ -1,23 +1,31 @@
 {
   "builders": [{
     "type": "amazon-ebs",
-    "access_key": "",
-    "secret_key": "",
+    "access_key": "AKIAI6N6DQNDGTZOXBWA",
+    "secret_key": "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",
     "region": "us-east-1",
-    "source_ami": "ami-5b5c1632",
+    "source_ami": "ami-31104658",
     "instance_type": "t1.micro",
     "ssh_username": "ubuntu",
     "ami_name": "awx-13 {{timestamp}}"
   }],
   "provisioners": [{
+     "type": "file",
+     "source": "rc.local",
+     "destination": "/tmp/rc.local"
+   },
+   {
     "type": "shell",
     "inline": [
       "sleep 30",
+      "sudo wget --no-check-certificate http://s3.amazonaws.com/awx_ami_script/awx_reset.yml -O /root/awx_reset.yml",
+      "sudo cp /tmp/rc.local /etc/rc.local",
+      "sudo chmod 755 /tmp/rc.local",
       "sudo apt-get update",
       "sudo apt-get install -y ansible",
-      "wget http://www.ansibleworks.com/releases/awx/setup/awx-setup-release_1.3.0.tar.gz",
-      "tar -xvzf awx-setup-release_1.3.0.tar.gz",
-      "cd awx-setup-release_1.3.0",
+      "wget http://www.ansibleworks.com/releases/awx/setup/awx-setup-1.3.1.tar.gz",
+      "tar -xvzf awx-setup-1.3.1.tar.gz",
+      "cd awx-setup-1.3.1",
       "sudo ./setup.sh"
     ]
   }]
